8d99d12aa7688936d122f5eea30ce8e1a4bcd3c1
Seann8 <seannolan85@gmail.com>
2015-12-01 13:53:03 +0000

    added hockey.py for next challenge

---
+++ hockey.py
@@ -0,0 +1,21 @@
+from getchallenge import challenge
+
+
+clue = "hockey.html"
+
+problem = challenge(clue)
+print(problem.text)
+
+'''
+Its in the air . look at the letters  if we look at the letters
+that make up hockey they spell oxygen  so lets try that one
+'''
+clue2 = 'oxygen.html'
+
+problem = challenge(clue2)
+print(problem.text)
+
+'''
+a png file , looks like image processing may be needed
+
+'''

36f042a1fb2fc4668a7a6e885bf65b6c37c5058a
Seann8 <seannolan85@gmail.com>
2015-12-02 23:15:29 +0000

    added some comments

--- hockey.py
+++ hockey.py
@@ -1,14 +1,15 @@
 from getchallenge import challenge
 from PIL import Image
 from io import BytesIO
+
 clue = "hockey.html"
 
 problem = challenge(clue)
 print(problem.text)
 
 '''
-Its in the air . look at the letters  if we look at the letters
-that make up hockey they spell oxygen  so lets try that one
+Its in the air. Look at the letters if we look at the letters
+that make up hockey they spell oxygen so lets try that one
 '''
 
 clue2 = 'oxygen.html'
@@ -17,11 +18,15 @@ problem = challenge(clue2)
 print(problem.text)
 
 '''
-a png file , looks like image processing may be needed
+a png file, looks like image processing may be needed
 so the image has a band of grey pixels this bad must be my encoded data
-I need to figure out a way to read it
+I need to figure out a way to read it.
 '''
+
 clue3 = 'oxygen.png'
 problem = challenge(clue3)
 
 pic = Image.open(BytesIO(problem.content))
+# loaded image into an image object , now the real work begins :P
+
+
